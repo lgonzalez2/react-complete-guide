828866f43940e7c67561a3a011933e755cb89b26
lgonzalez2 <lgonzalez_94@hotmail.com>
2022-08-03 15:52:35 -0500

    add ExpensesFilter component to select a year via drop down and effect state in Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,27 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses (props) {
+    const [selectedYear, setSelectedYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setSelectedYear(selectedYear);
+    }
+
     return (
-        <Card className="expenses">
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}></ExpenseItem>
-        </Card>
+        <div>
+            <ExpensesFilter selected={selectedYear} onChangeFilter={filterChangeHandler}/>
+            <Card className="expenses">
+                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
+                <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
+                <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
+                <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
+            </Card>
+        </div>
     );
 }
 
