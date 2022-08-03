602fd2dcbe5aaf9ef2ab5b74c9b61cfcf9099af4
lgonzalez2 <lgonzalez_94@hotmail.com>
2022-08-03 12:08:11 -0500

    add clickHandler function to change title on the click of button, and this uses useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+    
+    const clickHandler = () => {
+        setTitle('Updated!');
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item-description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item-price">{props.amount}</div>
             </div>
+            {/* dont use parentheses on click handler 
+            because then the function will be done at 
+            initial rendering, not when event actually occurs */}
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
