0efedaf956009f4be338e9835c925727ae5922a6
lgonzalez2 <lgonzalez_94@hotmail.com>
2022-08-06 10:43:21 -0500

    add key attribute to ExpenseItem so React is able to uniquely identify ExpenseItems rendered and there are no bugs when setting the expenses in App state with the new expense and spread of prevEExpenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,12 +14,15 @@ function Expenses (props) {
 
     return (
         <div>
-            <ExpensesFilter selected={selectedYear} onChangeFilter={filterChangeHandler}/>
             <Card className="expenses">
-                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-                <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-                <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
-                <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
+                <ExpensesFilter selected={selectedYear} onChangeFilter={filterChangeHandler}/>
+                {props.expenses.map(expense => (
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date}/>
+                ))}
             </Card>
         </div>
     );
